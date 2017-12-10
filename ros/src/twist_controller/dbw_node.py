d04154822a65d9da3c16969f514e20282592214e
swaroopsss2008 <swaroopsss2008@gmail.com>
2017-12-10 14:23:50 +0000

    [SK] - chnages done to handle break , steering and throttle based on deadband . Also , I also tuned lowpass filter parameters to handle sudden change in accelartion gradually . Also , braking pedal type is made of type percentage and already done for steering. Also percentage factor 1000 is giving shape braking when traffic light turns close to stop line

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -10,25 +10,19 @@ from twist_controller import Controller
 
 '''
 You can build this node only after you have built (or partially built) the `waypoint_updater` node.
-
 You will subscribe to `/twist_cmd` message which provides the proposed linear and angular velocities.
 You can subscribe to any other message that you find important or refer to the document for list
 of messages subscribed to by the reference implementation of this node.
-
 One thing to keep in mind while building this node and the `twist_controller` class is the status
 of `dbw_enabled`. While in the simulator, its enabled all the time, in the real car, that will
 not be the case. This may cause your PID controller to accumulate error because the car could
 temporarily be driven by a human instead of your controller.
-
 We have provided two launch files with this node. Vehicle specific values (like vehicle_mass,
 wheel_base) etc should not be altered in these files.
-
 We have also provided some reference implementations for PID controller and other utility classes.
 You are free to use them or build your own.
-
 Once you have the proposed throttle, brake, and steer values, publish it on the various publishers
 that we have created in the `__init__` function.
-
 '''
 
 class DBWNode(object):
@@ -69,7 +63,8 @@ class DBWNode(object):
 		'max_steer_angle': max_steer_angle,
 		'decel_limit':     decel_limit,
 		'accel_limit':     accel_limit,
-		'sample_rate':	   self.sample_rate
+		'sample_rate':	   self.sample_rate,
+                'deadband'   :     brake_deadband
 	}
 	self.controller = Controller(**controller_params)
 
@@ -113,6 +108,12 @@ class DBWNode(object):
         bcmd = BrakeCmd()
         bcmd.enable = True
         bcmd.pedal_cmd_type = BrakeCmd.CMD_TORQUE
+        bcmd.pedal_cmd = brake
+
+
+        bcmd.pedal_cmd_type = BrakeCmd.CMD_PERCENT
+        if brake_deadband > 0.1:
+            brake *= 1000  # multiplier for sim
         bcmd.pedal_cmd = brake
         self.brake_pub.publish(bcmd)
 
--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -12,28 +12,44 @@ class Controller(object):
         # TODO: Implement
 	wheel_base = kwargs["wheel_base"]
 	steer_ratio = kwargs["steer_ratio"]
-	self.min_speed = 5.0
+	self.min_speed = 0.0
 	max_lat_accel = kwargs["max_lat_accel"]
 	max_steer_angle = kwargs["max_steer_angle"]
 	decel_limit = kwargs["decel_limit"]
 	accel_limit = kwargs["accel_limit"]
 	self.sample_rate = kwargs["sample_rate"]
-	self.pid = PID(1.0, 0.012, 0.1, decel_limit, accel_limit)
+        self.deadband    = kwargs['deadband']
+	self.pid = PID(0.5, 0.012, 0.1, decel_limit, accel_limit)
 	self.yaw_controller = YawController(wheel_base, steer_ratio, self.min_speed, max_lat_accel, max_steer_angle)
-	self.filter_steer = LowPassFilter(0.1, 0.2)
-	self.filter_throttlebrake = LowPassFilter(0.2, 0.1)
+	self.filter_throttlebrake = LowPassFilter(0.07, 0.02)
 
     def control(self, target_linear_velocity, target_angular_velocity, current_linear_velocity, dbwenabled):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
-	if (not dbwenabled):
-		self.pid.reset()
-	throttle_brake = self.pid.step(target_linear_velocity - current_linear_velocity, 1.0/self.sample_rate)
-	throttle_brake = self.filter_throttlebrake.filt(throttle_brake)
-	throttle = max(throttle_brake, 0)
-	brake = min(throttle_brake, 0)
+
+        brake = 0.0
+        throttle = 0.0
+        #sampling fequency as suggested by udacity
+	throttle_brake = self.pid.step(target_linear_velocity - current_linear_velocity, 1.0/50)
+	# handling boundary conditions for throttle 
+	if throttle_brake > 0.:
+            throttle = throttle_brake
+            throttle = self.filter_throttlebrake.filt(throttle)
+        elif throttle_brake < -self.deadband:
+            throttle = 0.
+            brake = -throttle_brake
+
 	steer = self.yaw_controller.get_steering(target_linear_velocity, target_angular_velocity, current_linear_velocity)
-	if (current_linear_velocity > self.min_speed):
-		steer = self.filter_steer.filt(steer)
+        # handling boundary conditions for steer  based on deadband  
+        if self.deadband < 0.1:
+            steer = target_angular_velocity * steer_ratio 
+
+        # handling boundary conditions for brake based on deadband at deadstop condition   
+        if (target_linear_velocity <= 0.01) and (brake < self.deadband):     
+            brake = self.deadband
+        # reset PID when DBW not enabled
+        if (not dbwenabled):
+	    self.pid.reset()
+
 	#rospy.loginfo('Controller - error:%f, throttle:%f, brake:%f, steer:%f', target_linear_velocity - current_linear_velocity, throttle, brake, steer)
 	return throttle, brake, steer
