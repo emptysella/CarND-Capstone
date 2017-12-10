7369cc16bc7b7daadc59a8f77263ec0f7e6e48d4
swaroopsss2008 <swaroopsss2008@gmail.com>
2017-12-10 18:44:33 +0000

    [vk] - venkat clean up

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -12,24 +12,6 @@ import numpy as np
 import tf
 
 
-
-'''
-This node will publish waypoints from the car's current position to some `x` distance ahead.
-As mentioned in the doc, you should ideally first implement a version which does not care
-about traffic lights or obstacles.
-Once you have created dbw_node, you will update this node to use the status of traffic lights too.
-Please note that our simulator also provides the exact location of traffic lights and their
-current status in `/vehicle/traffic_lights` message. You can use this message to build this node
-as well as to verify your TL classifier.
-'''
-
-PUBLISHING_RATE = 2 # Publishing frequency (Hz)
-
-class WaypointUpdater(object):
-    def __init__(self):
-        rospy.init_node('waypoint_updater')#!/usr/bin/env python
-
-
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
 As mentioned in the doc, you should ideally first implement a version which does not care
@@ -42,7 +24,7 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
-
+PUBLISHING_RATE = 2 # Publishing frequency (Hz)
 
 class WaypointUpdater(object):
     def __init__(self):
