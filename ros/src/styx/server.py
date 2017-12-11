4d23349958fca9dc6e1c10d2ebf7ba1708e90aea
swaroopsss2008 <swaroopsss2008@gmail.com>
2017-12-11 14:00:30 +0000

    [sk] - simulator lag solved

--- ros/src/styx/server.py
+++ ros/src/styx/server.py
@@ -4,6 +4,7 @@ import socketio
 import eventlet
 import eventlet.wsgi
 import time
+import rospy
 from flask import Flask, render_template
 
 from bridge import Bridge
@@ -11,7 +12,10 @@ from conf import conf
 
 sio = socketio.Server()
 app = Flask(__name__)
-msgs = []
+
+msgs = {}
+# Reduce simulator lag
+# https://github.com/udacity/self-driving-car-sim/issues/53
 
 dbw_enable = False
 
@@ -20,9 +24,7 @@ def connect(sid, environ):
     print("connect ", sid)
 
 def send(topic, data):
-    s = 1
-    msgs.append((topic, data))
-    #sio.emit(topic, data=json.dumps(data), skip_sid=True)
+    msgs[topic] = data
 
 bridge = Bridge(conf, send)
 
@@ -34,28 +36,39 @@ def telemetry(sid, data):
         bridge.publish_dbw_status(dbw_enable)
     bridge.publish_odometry(data)
     for i in range(len(msgs)):
-        topic, data = msgs.pop(0)
+        topic, data = msgs.popitem()
         sio.emit(topic, data=data, skip_sid=True)
 
-@sio.on('control')
+#@sio.on('control')
 def control(sid, data):
-    bridge.publish_controls(data)
+    #bridge.publish_controls(data)
+    pass
 
-@sio.on('obstacle')
+#@sio.on('obstacle')
 def obstacle(sid, data):
-    bridge.publish_obstacles(data)
+    #bridge.publish_obstacles(data)
+    #rospy.loginfo("obstacle callback")
+    pass
 
-@sio.on('lidar')
+#@sio.on('lidar')
 def obstacle(sid, data):
-    bridge.publish_lidar(data)
-
+    #bridge.publish_lidar(data)
+    pass
+    
 @sio.on('trafficlights')
 def trafficlights(sid, data):
     bridge.publish_traffic(data)
 
+framecount = 0
+framesample = 2
 @sio.on('image')
 def image(sid, data):
-    bridge.publish_camera(data)
+    global count
+    framecount += 1
+    if framecount%(framesample+1)==0:
+        bridge.publish_camera(data)
+    #rospy.loginfo("imagesize: %d", len(data["image"]) )
+    #pass
 
 if __name__ == '__main__':
 
